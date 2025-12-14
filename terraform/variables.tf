26670b0c1e8fb2831b020c67bfa222820632f07e
tagore8661 <tagore8661@gmail.com>
2025-12-14 13:56:48 +0530

    variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "name" {
+  description = "Name prefix for resources"
+  type        = string
+  default     = "ZustPe"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR block for private subnet 1"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR block for private subnet 2"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type "
+  type        = string
+  default     = "t2.micro" 
+}
